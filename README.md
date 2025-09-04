77d75c9bed697660efda680bae1912a32ec7527b
yeahokthen <jen.edgarz@gmail.com>
2025-09-04 01:01:03 +0100

    adding flyway

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web-catalogue-service
+
+docker run -d --name polar-postgres -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=polarDB_catalogue -p 5432:5432 postgres:17.0
---
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE book
+(
+    id                 BIGSERIAL PRIMARY KEY NOT NULL,
+    author             VARCHAR(255)          not null,
+    isbn               varchar(255) UNIQUE   NOT NULL,
+    price              float                 NOT NULL,
+    title              varchar(255)          NOT NULL,
+    created_date       timestamp,
+    last_modified_date timestamp,
+    version            integer               NOT NULL
+);
